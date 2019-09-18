e58e6edf743ddcb79b8ee2dcdf8a99adeb65a5c8
DateTraveller <982902852@qq.com>
2019-09-18 22:45:01 +0800

    新建GIT库

---
+++ BoilMyWater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoilMyWater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
