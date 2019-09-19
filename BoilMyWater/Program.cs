291dd5911e8c8f65e80df6894759a5a7f5eee176
DateTraveller <982902852@qq.com>
2019-09-19 17:14:34 +0800

    log4net

--- BoilMyWater/Program.cs
+++ BoilMyWater/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using log4net;
+using log4net.Config;
+using log4net.Repository;
 
 namespace BoilMyWater
 {
@@ -10,6 +14,24 @@ namespace BoilMyWater
             Nullable<int> a1 = null;
             Console.WriteLine("Hello World!");
             Console.WriteLine(a.GetValueOrDefault());
+
+            InitLog4Net();
+            var logger = LogManager.GetLogger(typeof(Program));
+
+            logger.Info("消息");
+            logger.Warn("警告");
+            logger.Error("异常");
+            logger.Fatal("错误");
+
+            Console.ReadLine();
+
+        }
+
+        private static void InitLog4Net()
+        {
+            ILoggerRepository repository = LogManager.CreateRepository("NETCoreRepository");
+            var logCfg = new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config");
+            XmlConfigurator.ConfigureAndWatch(repository,logCfg);
         }
     }
 }
