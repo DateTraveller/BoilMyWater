655bf84969276abfd9a471047f5062b771a077a0
DateTraveller <982902852@qq.com>
2019-09-19 21:23:59 +0800

    简介和基础

--- BoilMyWater/Program.cs
+++ BoilMyWater/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using BoilMyWater._1_2.简介和基础;
+using System;
 
 namespace BoilMyWater
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            MethodFortheFirst();
             Console.WriteLine("Hello World!");
+        }
+
+        private static void MethodFortheFirst()
+        {
+            UnitConverter uc = new UnitConverter(10);
+            Console.WriteLine(uc.Convert(2));
         }
     }
 }
