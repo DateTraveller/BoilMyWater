b853ae9ead47bec3e5f5e069e26be149d74c98ba
DateTraveller <982902852@qq.com>
2019-12-16 23:15:22 +0800

    EFCore

---
+++ EFCore.DomainModels/City.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace EFCore.DomainModels
+{
+    public class City
+    {
+        public City()
+        {
+            CityCompanies = new List<CityCompany>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AreaCode { get; set; }
+
+        public int ProvinceId { get; set; }
+        public Province province { get; set; }
+
+        public List<CityCompany> CityCompanies { get; set; }
+
+        public Mayor Mayor { get; set; }
+    }
+}
---
+++ EFCore.DomainModels/Mayor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EFCore.DomainModels
+{
+    public class Mayor
+    {
+        public int Id { get; set; }
+        public int CityId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
+        public City City { get; set; }
+    }
+}
